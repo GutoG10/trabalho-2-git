7c247ed99d58330ee3618040e60ee5eab2565ef0
Joao Pedro Carlos da Silva <joaopcarlosdasilva@gmail.com>
2024-07-01 18:09:17 -0300

    commit filtro

--- React/src/components/EntriesList.js
+++ React/src/components/EntriesList.js
@@ -13,15 +13,15 @@ const EntriesList = ({ startDate, endDate }) => {
   useEffect(() => {
     const fetchEntries = async () => {
       try {
-        const response = await axios.get(API_URL, {
-          params: {
-            startDate,
-            endDate,
-          },
+        const response = await axios.get(API_URL);
+        // Filtra os lançamentos com base nas datas de vencimento
+        const filteredEntries = response.data.filter(entry => {
+          const dueDate = new Date(entry.due_date);
+          return dueDate >= new Date(startDate) && dueDate <= new Date(endDate);
         });
 
-        // Ordena os lançamentos pela data de vencimento (due_date)
-        const sortedEntries = response.data.sort((a, b) => {
+        // Ordena os lançamentos filtrados pela data de vencimento (due_date)
+        const sortedEntries = filteredEntries.sort((a, b) => {
           if (a.due_date < b.due_date) return -1;
           if (a.due_date > b.due_date) return 1;
           return 0;
@@ -70,42 +70,49 @@ const EntriesList = ({ startDate, endDate }) => {
 
   return (
     <div className="flex flex-col items-center justify-center w-full">
-        <h1 className="text-[30px]">Lista De Lançamentos</h1>
-        <button onClick={() => Router.push("/admin/entries/create")} className="m-2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" type="button" placeholder="Criar Lançamento">Criar Lançamento</button>
+      <h1 className="text-[30px]">Lista De Lançamentos</h1>
+      <button
+        onClick={() => Router.push("/admin/entries/create")}
+        className="m-2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+        type="button"
+        placeholder="Criar Lançamento"
+      >
+        Criar Lançamento
+      </button>
 
-        <table>
-          <thead>
-            <tr className="bg-gray-200">
-              <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">Tipo</th>
-              <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">Categoria</th>
-              <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">Descrição</th>
-              <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">Valor</th>
-              <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">Data de Pagamento</th>
-              <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">Data de Vencimento</th>
-              <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">Conta Relacionada</th>
-              <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">Status</th>
-              <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">Ações</th>
+      <table>
+        <thead>
+          <tr className="bg-gray-200">
+            <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">Tipo</th>
+            <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">Categoria</th>
+            <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">Descrição</th>
+            <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">Valor</th>
+            <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">Data de Pagamento</th>
+            <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">Data de Vencimento</th>
+            <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">Conta Relacionada</th>
+            <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">Status</th>
+            <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">Ações</th>
+          </tr>
+        </thead>
+        <tbody id="tabelaLancamentos">
+          {entries.map((entry) => (
+            <tr className="bg-gray-300" key={entry._id}>
+              <td className="p-2 border-solid border-2 border-gray-400">{categoryMap[entry.category] ? categoryMap[entry.category].type : ""}</td>
+              <td className="p-2 border-solid border-2 border-gray-400">{categoryMap[entry.category] ? categoryMap[entry.category].description : "--Deletado--"}</td>
+              <td className="p-2 border-solid border-2 border-gray-400">{entry.description}</td>
+              <td className="p-2 border-solid border-2 border-gray-400">R${parseFloat(entry.value).toFixed(2)}</td>
+              <td className="p-2 border-solid border-2 border-gray-400">{entry.payment_date}</td>
+              <td className="p-2 border-solid border-2 border-gray-400">{entry.due_date}</td>
+              <td className="p-2 border-solid border-2 border-gray-400">{accountMap[entry.account] || "--Deletado--"}</td>
+              <td className="p-2 border-solid border-2 border-gray-400">{entry.status}</td>
+              <td className="p-2 border-solid border-2 border-gray-400">
+                <EntryAction pid={entry._id}></EntryAction>
+              </td>
             </tr>
-          </thead>
-          <tbody id="tabelaLancamentos">
-            {entries.map(entry => (
-              <tr className="bg-gray-300" key={entry._id}>
-                <td className="p-2 border-solid border-2 border-gray-400">{categoryMap[entry.category] ? categoryMap[entry.category].type : ""}</td>
-                <td className="p-2 border-solid border-2 border-gray-400">{categoryMap[entry.category] ? categoryMap[entry.category].description : "--Deletado--"}</td>
-                <td className="p-2 border-solid border-2 border-gray-400">{entry.description}</td>
-                <td className="p-2 border-solid border-2 border-gray-400">R${parseFloat(entry.value).toFixed(2)}</td>
-                <td className="p-2 border-solid border-2 border-gray-400">{entry.payment_date}</td>
-                <td className="p-2 border-solid border-2 border-gray-400">{entry.due_date}</td>
-                <td className="p-2 border-solid border-2 border-gray-400">{accountMap[entry.account] || "--Deletado--"}</td>
-                <td className="p-2 border-solid border-2 border-gray-400">{entry.status}</td>
-                <td className="p-2 border-solid border-2 border-gray-400">
-                  <EntryAction pid={ entry._id }></EntryAction>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
--- React/src/pages/admin/entries/index.js
+++ React/src/pages/admin/entries/index.js
@@ -1,67 +1,20 @@
 import React, { useEffect, useState } from "react";
-import Router from "next/router";
 import HeadAdm from "@/components/HeadAdm";
-import axios from "axios";
 import Head from "next/head";
-import EntryAction from "@/components/EntryAction";
 import EntriesList from "@/components/EntriesList";
 
 export default function Entries() {
-  const API_URL = "http://localhost:8080/api/lancamentos";
-  const [entries, setEntries] = useState([]);
-  const [categoryMap, setCategoryMap] = useState({});
-  const [accountMap, setAccountMap] = useState({});
-
-  useEffect(() => {
-    const getAllEntries = async () => {
-      try {
-        const response = await axios.get(API_URL);
-        const updatedEntries = response.data.map(entry => ({
-          ...entry,
-          type: categoryMap[entry.category] ? categoryMap[entry.category].type : ""
-        }));
-        setEntries(updatedEntries);
-      } catch (error) {
-        console.error('Erro ao buscar os lançamentos:', error);
-      }
-    };
-
-    const getAllCategories = async () => {
-      try {
-        const response = await axios.get("http://localhost:8080/api/categorias");
-        const categoryMap = response.data.reduce((map, category) => {
-          map[category._id] = category;
-          return map;
-        }, {});
-        setCategoryMap(categoryMap);
-      } catch (error) {
-        console.error("Erro ao buscar as categorias:", error);
-      }
-    };
-
-    const getAllAccounts = async () => {
-      try {
-        const response = await axios.get("http://localhost:8080/api/contas");
-        const accountMap = response.data.reduce((map, account) => {
-          map[account._id] = account.description;
-          return map;
-        }, {});
-        setAccountMap(accountMap);
-      } catch (error) {
-        console.error("Erro ao buscar as contas:", error);
-      }
-    };
-
-    getAllCategories();
-    getAllAccounts();
-    getAllEntries();
-  }, []);
+  // Defina as datas de início e fim aqui (por exemplo)
+  const startDate = "2024-01-01"; // data de início
+  const endDate = "2024-12-31"; // data de fim
 
   return (
     <div className="min-h-screen bg-gray-100">
-      <Head><title>Lista de Lançamentos</title></Head>
-      <HeadAdm></HeadAdm>
-      <EntriesList></EntriesList>
+      <Head>
+        <title>Lista de Lançamentos</title>
+      </Head>
+      <HeadAdm />
+      <EntriesList startDate={startDate} endDate={endDate} />
     </div>
   );
 }
